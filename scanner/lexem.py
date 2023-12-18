888324c71ed6c913aa69183003f2d69c0fa2e7a7
Dan <lyatorovskiy@gmail.com>
2023-12-18 23:49:49 +0300

    Cleaning

--- scanner/lexem.py
+++
@@ -1,7 +0,0 @@
-class Lexem:
-    def __init__(self, token, value=None) -> None:
-        self.token = token
-        self.value = value
-
-    def __str__(self):
-        return f"{self.token}({self.value})"
